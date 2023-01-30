11afe919095d6ff055ba1db9b7002ee90d3e70c2
root <root@localhost.localdomain>
2023-01-30 13:00:57 -0500

    updates

---
+++ events.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css">
+  <title>Events</title>
+ </head>
+ <body>
+ <?php
+        include 'includes/header.inc.php';
+    ?>
+<p>Hi!</p>
+ <body>
+</html>
---
+++ faq.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css">
+  <title>FAQ</title>
+ </head>
+ <body>
+ <?php
+        include 'includes/header.inc.php';
+    ?>
+<p>Ok</p>
+ <body>
+</html>
